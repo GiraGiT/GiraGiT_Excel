353136e36e124c0d70bb6ecb305ce186a97e6b06
GiraGiT <65813708+GiraGiT@users.noreply.github.com>
2024-03-03 19:27:38 +0500

    Fine-tuned Cell Formatting
    Added validation by the value of numbers in cells:
    
    If numbers have a fractional part separated by "." or ",", a custom format is set.
    
    Otherwise, the "General" format will be set.

--- All_signs_after_the_comma.vb
+++ All_signs_after_the_comma.vb
@@ -1,14 +1,19 @@
 Sub All_signs_after_the_comma()
     ' Проверка, есть ли выделенные ячейки
-    If Selection.Count = 0 Then
+    If Selection.count = 0 Then
         MsgBox "Нет выделенных ячеек", vbInformation
         Exit Sub
     End If
 
     ' Применение пользовательского формата к выделенным ячейкам
-    Selection.NumberFormat = "0.#############################"
+    For Each cell In Selection
+        If IsNumeric(cell.Value) And (InStr(cell.Value, ".") > 0 Or InStr(cell.Value, ",") > 0) Then
+            cell.NumberFormat = "0.##########################################################"
+        Else
+            cell.NumberFormat = "General"
+        End If
+    Next cell
 End Sub
-
 
 Sub CallAll_signs_after_the_comma(control As IRibbonControl)
     Call All_signs_after_the_comma
