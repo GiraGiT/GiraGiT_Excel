2c5e098f10b22681b4eea68f1de10d40c8ae635d
GiraGiT <arslan.faleev@gmail.com>
2024-11-25 19:55:25 +0500

    Fix issues with function NumToText

--- NumToText.vb
+++ NumToText.vb
@@ -1,53 +1,59 @@
 Option Explicit
 Public Function NumToText(chislo As Double, Optional rod As Integer = 1) As String
-    '
-    ' Автоматическое определение целого и дробного числа
-    ' Если число целое, используется переданный аргумент для определения рода
-
     Dim drobLen As Variant, x2 As Double, utr As String, utr1, ITOG0 As String, itog As String
     Dim tekst As String, sklon As String, drobprop As String
-    If chislo > 999999999999.99 Then
-        NumToText = "Аргумент больше 999 999 999 999.99!"
+    
+    If chislo > 1E+15 Then
+        NumToText = "Аргумент больше 999 999 999 999 999.99!"
+        Exit Function
     ElseIf chislo < 0 Then
         NumToText = "Аргумент отрицательный!"
+        Exit Function
     End If
 
     ' Проверка на наличие дробной части
     If chislo = Int(chislo) Then
         ' Целое число
         chislo = Int(chislo)
-        ITOG0 = СуммаOPC(chislo, 1, rod)
+        ITOG0 = Trim(СуммаOPC(chislo, 1, rod))
     Else
         ' Дробное число
-        x2 = CDbl(Mid(chislo, InStr(1, chislo, ",") + 1))
+        Dim целаяЧасть As Double, дробнаяЧасть As Double
+        целаяЧасть = Int(chislo)
+        дробнаяЧасть = CDbl(Mid(chislo, InStr(1, chislo, ",") + 1))
+        
         utr = str(chislo)
         utr1 = Split(utr, ".")
         drobLen = Len(utr1(1))
-        itog = СуммаOPC(Int(chislo), 3, 1) ' Мужской род используется по умолчанию для дробной части
-        drobprop = СуммаOPC(Int(x2), 3, 1) ' Мужской род используется по умолчанию для дробной части
-        If Right(Int(chislo), 1) = "1" Then
-            tekst = tekst & " целая "
+        
+        itog = Trim(СуммаOPC(целаяЧасть, 3, 1)) ' Мужской род для целой части
+        drobprop = Trim(СуммаOPC(Int(дробнаяЧасть), 3, 1)) ' Мужской род для дробной части
+        
+        ' Правильное склонение "целых/целая"
+        If целаяЧасть = 1 Then
+            tekst = " целая "
         Else
-            tekst = tekst & " целых "
+            tekst = " целых "
         End If
-        sklon = drobnaya(x2, drobLen)
-        ITOG0 = itog & tekst & drobprop & sklon
+        
+        sklon = drobnaya(дробнаяЧасть, drobLen)
+        ITOG0 = Trim(itog & tekst & drobprop & " " & sklon)
     End If
 
-    ' Преобразование первой буквы в заглавную
+    ' Преобразование первой буквы в заглавную и удаление лишних пробелов
     NumToText = UCase(Left(ITOG0, 1)) & Mid(ITOG0, 2)
 End Function
 
 Public Function СуммаOPC(x As Double, opc As Long, rod As Integer) As String
-    Dim y(1 To 4) As Integer, i1 As Byte
-    Dim Text(1 To 4) As String, i2 As Byte, y1 As Byte, y2 As Byte, _
+    Dim y(1 To 5) As Integer, i1 As Byte
+    Dim Text(1 To 5) As String, i2 As Byte, y1 As Byte, y2 As Byte, _
             y3 As Byte, Text0 As String, text1 As String, text2 As String, Text3 As String, _
-            Text4 As String
-    For i1 = 1 To 4
+            Text4 As String, Text5 As String
+    For i1 = 1 To 5
         x = Fix(x) / 1000
         y(i1) = (x - Fix(x)) * 1000
     Next
-    For i2 = 1 To 4
+    For i2 = 1 To 5
         y1 = y(i2) Mod 10
         y2 = (y(i2) - y1) / 10 Mod 10
         y3 = y(i2) \ 100
@@ -59,48 +65,51 @@ Public Function СуммаOPC(x As Double, opc As Long, rod As Integer) As Strin
             Text3 = Choose(y1 + 1, "десять ", "одиннадцать ", "двенадцать ", _
                     "тринадцать ", "четырнадцать ", "пятнадцать ", "шестнадцать ", _
                     "семнадцать ", "восемнадцать ", "девятнадцать ")
-        ElseIf y2 <> 1 And i2 = 2 Then
-            Text3 = Choose(y1 + 1, "", IIf(rod = 2, "одна ", "один "), "две ", "три ", "четыре ", "пять ", _
-                    "шесть ", "семь ", "восемь ", "девять ")
         Else
-            If opc = 2 Or opc = 3 Then
-                Text3 = Choose(y1 + 1, "", "одна ", "две ", "три ", "четыре ", "пять ", _
+            If y2 <> 1 Then
+                Text3 = Choose(y1 + 1, "", IIf(i2 = 2, "одна ", "один "), "две ", "три ", "четыре ", "пять ", _
                         "шесть ", "семь ", "восемь ", "девять ")
             Else
-                Text3 = Choose(y1 + 1, "", IIf(rod = 2, "одна ", "один "), "два ", "три ", "четыре ", "пять ", _
+                Text3 = Choose(y1 + 1, "", IIf(rod = 2, "одна ", "один "), "один ", "два ", "три ", "четыре ", "пять ", _
                         "шесть ", "семь ", "восемь ", "девять ")
             End If
         End If
         If y2 <> 1 And y1 = 1 Then
-            Text4 = Choose(i2, "", "тысяча ", "миллион ", "миллиард ")
+            Text4 = Choose(i2, "", "тысяча ", "миллион ", "миллиард ", "триллион ")
         ElseIf y2 <> 1 And y1 > 1 And y1 < 5 Then
-            Text4 = Choose(i2, "", "тысячи ", "миллиона ", "миллиарда ")
+            Text4 = Choose(i2, "", "тысячи ", "миллиона ", "миллиарда ", "триллиона ")
         ElseIf y1 = 0 And y2 = 0 And y3 = 0 Then
-            Text4 = Choose(i2, "", "", "", "")
+            Text4 = Choose(i2, "", "", "", "", "")
         Else
-            Text4 = Choose(i2, "", "тысяч ", "миллионов ", "миллиардов ")
+            Text4 = Choose(i2, "", "тысяч ", "миллионов ", "миллиардов ", "триллионов ")
         End If
         Text(i2) = text1 & text2 & Text3 & Text4
     Next
-    If y(1) + y(2) + y(3) + y(4) = 0 Then
+    If y(1) + y(2) + y(3) + y(4) + y(5) = 0 Then
         Text0 = "ноль "
     Else
-        Text0 = Text(4) & Text(3) & Text(2) & Text(1)
+        Text0 = Text(5) & Text(4) & Text(3) & Text(2) & Text(1)
     End If
     СуммаOPC = Text0
 End Function
 
 Public Function drobnaya(x2 As Double, drobLen As Variant) As String
     Dim x As Variant, scl As String
+    
+    ' Максимальная длина - триллионные
+    If drobLen > 12 Then
+        drobLen = 12
+    End If
+    
     x = Right(x2, 1)
     If x = 1 Then
         scl = Choose(drobLen, "десятая", "сотая", "тысячная", "десятитысячная", _
                 "стотысячная", "миллионная", "десятимиллионная", "стомиллионная", _
-                "миллиардная", "десятимиллиардная")
+                "миллиардная", "десятимиллиардная", "стомиллиардная", "триллионная")
     Else
         scl = Choose(drobLen, "десятых", "сотых", "тысячных", "десятитысячных", _
                 "стотысячных", "миллионных", "десятимиллионных", "стомиллионных", _
-                "миллиардных", "десятимиллиардных")
+                "миллиардных", "десятимиллиардных", "стомиллиардных", "триллионных")
     End If
     drobnaya = scl
 End Function
@@ -118,7 +127,7 @@ Sub InsertFormula()
     If первоеЗначение Is Nothing Then Exit Sub ' Если не выбрана ячейка, выходим из макроса
 
     ' Проверка на наличие дробной части
-    If первоеЗначение.Value = Int(первоеЗначение.Value) Then
+    If первоеЗначение.value = Int(первоеЗначение.value) Then
         ' Выбор рода для прописи целых чисел
         rod = Application.InputBox("Выберите род для прописи целых чисел: 1 - мужской, 2 - женский", Type:=1)
         If rod < 1 Or rod > 2 Then Exit Sub ' Если род выбран неверно, выходим из макроса
